5f5ae1b5000f1c256f7f8b662ea79888dffe7a8e
Muhammad Dawood Iqbal <dawoodjand356@gmail.com>
2025-12-20 21:34:46 +0500

    update and debug

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://ai-humanoid-robotics-book.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/physical-ai-book/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
